5fc2aaf1216c44af14242867c68b19726745cf2a
Fer Perales <fernando.perales@live.com.mx>
2012-09-28 21:07:28 -0500

    Archivos para la practica 11 agregados

---
+++ Practicas/Practica 11/js/validacionesRegistro.js
@@ -0,0 +1,28 @@
+window.onload = function(){
+	
+	document.getElementById("enviar").onclick = validar;
+	
+	function validar(){
+		var form = document.registro;
+		if(form.razonMiembro.checked == false &&
+			form.razonGustaria.checked == false &&
+			form.razonInvitacion.checked == false &&
+			form.razonReferido.checked == false){		
+				alert("Debes seleccionar al menos una opción para inscribirte");				
+		}else if(form.password.value.length == 0){
+			alert("No puedes dejar el campo de contraseña vacío");
+		}else if(form.nombre.value.length == 0){
+			alert("No puedes dejar el campo de nombre vacío");
+			
+		}else if(form.direccion.value.length == 0){
+			alert("No puedes dejar el campo de dirección vacío");
+		}else if(form.telefono.value.length == 0){
+			alert("No puedes dejar el campo de teléfono vacío");			
+		}else if(form.nacimiento.value.length == 0){
+			alert("No puedes dejar el campo de fecha vacío");
+		}else{
+			form.submit();
+		}
+	}
+	
+}
