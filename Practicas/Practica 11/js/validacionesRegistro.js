da2d884144306ec651ad1a2078f540faa4e29cbd
Fer Perales <fernando.perales@live.com.mx>
2012-10-01 14:57:33 -0500

    Corregidas valicaciones del formulario

--- Practicas/Practica 11/js/validacionesRegistro.js
+++ Practicas/Practica 11/js/validacionesRegistro.js
@@ -2,27 +2,50 @@ window.onload = function(){
 	
 	document.getElementById("enviar").onclick = validar;
 	
+	function cambiarEstadoError(element, value){		
+		document.getElementById(element).style.visibility=value;		
+	}	
+	
 	function validar(){
 		var form = document.registro;
 		if(form.razonMiembro.checked == false &&
 			form.razonGustaria.checked == false &&
 			form.razonInvitacion.checked == false &&
 			form.razonReferido.checked == false){		
-				alert("Debes seleccionar al menos una opción para inscribirte");				
-		}else if(form.password.value.length == 0){
-			alert("No puedes dejar el campo de contraseña vacío");
-		}else if(form.nombre.value.length == 0){
-			alert("No puedes dejar el campo de nombre vacío");
-			
-		}else if(form.direccion.value.length == 0){
-			alert("No puedes dejar el campo de dirección vacío");
-		}else if(form.telefono.value.length == 0){
-			alert("No puedes dejar el campo de teléfono vacío");			
-		}else if(form.datepicker.value.length == 0){
-			alert("No puedes dejar el campo de fecha vacío");
+				cambiarEstadoError("error_razones", "visible");			
 		}else{
-			form.submit();
-		}
+				cambiarEstadoError("error_razones", "hidden");		
+		} 
+		
+		if(form.password.value.length == 0){
+			cambiarEstadoError("error_password", "visible");			
+		}else{
+			cambiarEstadoError("error_password", "hidden");		
+		} 
+		
+		if(form.nombre.value.length == 0){
+			cambiarEstadoError("error_nombre", "visible");			
+		}else{
+			cambiarEstadoError("error_nombre", "hidden");		
+		} 
+		
+		if(form.direccion.value.length == 0){
+			cambiarEstadoError("error_direccion", "visible");			
+		}else{
+			cambiarEstadoError("error_direccion", "hidden");		
+		} 
+		
+		if(form.telefono.value.length == 0){
+			cambiarEstadoError("error_telefono", "visible");			
+		}else{
+			cambiarEstadoError("error_telefono", "hidden");		
+		} 
+		
+		if(form.datepicker.value.length == 0){
+			cambiarEstadoError("error_nacimiento", "visible");			
+		}else{
+			cambiarEstadoError("error_nacimiento", "hidden");		
+		} 
 	}
 	
 }
